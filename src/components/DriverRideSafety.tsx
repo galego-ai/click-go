dcd39acd24c0152a864b32a5c1e109b4344fa5d2
galego-ai <elismarribeiro298@gmail.com>
2026-08-23 17:18:36 -0300

    Keep driver safety component props compatible while hidden

--- src/components/DriverRideSafety.tsx
+++ src/components/DriverRideSafety.tsx
@@ -1,5 +1,8 @@
 'use client'
 
-export default function DriverRideSafety(){
+type Loc={lat:number;lng:number}
+type Props={rideId:string;status:string;location:Loc|null;origin:Loc;destination:Loc}
+
+export default function DriverRideSafety(_props:Props){
  return null
 }
