8e65343242f2bb6e9fd57116561e7f732f508e46
galego-ai <elismarribeiro298@gmail.com>
2026-08-23 03:04:31 -0300

    Ativa olhos de senha em todo o sistema web

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
 import './globals.css'
 import AppShell from './AppShell'
+import PasswordEyes from '@/components/PasswordEyes'
 
 export const metadata = { title: 'CLICK-GO', description: 'Plataforma de mobilidade urbana CLICK-GO' }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return <html lang="pt-BR"><body><AppShell>{children}</AppShell></body></html>
+  return <html lang="pt-BR"><body><PasswordEyes/><AppShell>{children}</AppShell></body></html>
 }
